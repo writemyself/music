59ea7792a75f1790327368e06a4a816c4bd256cb
xiaosong <1562631196@qq.com>
2018-05-13 12:22:02 +0800

    配置angular后台

--- webClient/src/app/app.component.ts
+++ webClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private http: Http){
+    
+  }
+  getData(){
+    this.http.get("login/cellphone?phone=13213013241&password=xs110112").subscribe(data => {
+      console.log(data);
+    });
+  }
 }
